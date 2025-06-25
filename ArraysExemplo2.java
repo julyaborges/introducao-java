dac917ad5da227b911e3a73a0c9cf104a9634d07
Ana Julya Borges <borgesanajulya@gmail.com>
2025-06-25 11:45:38 -0300

    Arrays e Matrizes

--- ArraysExemplo2.java
+++ ArraysExemplo2.java
@@ -7,7 +7,7 @@ Informe se cada aluno foi aprovado ou reprovado (nota mínima para aprovação: 
 
 import java.util.Scanner;
 
-public class ArraysExemplo {
+public class ArraysExemplo2 {
 
     public static void main(String[] args){
         String[] alunos = new String[5];
@@ -16,15 +16,18 @@ public class ArraysExemplo {
         Scanner scanner = new Scanner(System.in);
 
         for(int i=0; i<5; i++){
-            System.out.println("Digite o nome do aluno " +(i+1)+": ");
+            System.out.println("Digite o nome do aluno: ");
             alunos[i] = scanner.nextLine();
 
             double notas = 0; 
-
-            for (int y=0; y<4; y++){
-                System.out.println("Digite a nota "+(y+1)+" do aluno "+alunos[i]+": ");
-                notas = notas + scanner.nextDouble();
-            }    
+            System.out.println("Digite a nota 1: "); 
+            notas = notas + scanner.nextDouble();
+            System.out.println("Digite a nota 2: "); 
+            notas = notas + scanner.nextDouble();
+            System.out.println("Digite a nota 3: "); 
+            notas = notas + scanner.nextDouble();
+            System.out.println("Digite a nota 4: "); 
+            notas = notas + scanner.nextDouble();
             
             scanner.nextLine(); //Limpa o buffer
 
---
+++ Matriz.java
@@ -0,0 +1,18 @@
+public class Matriz {
+
+    public static void main(String[] args){
+        int[][] matriz = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        for(int i=0; i<3; i++){
+            for(int y=0; y<3; y++){
+                System.out.print(matriz[i][y]+"\t|\t");
+            }
+            System.out.println("\n");
+
+        }
+    }
+}
